fcad3c049e2ca09f0eb3eff44f0e1af8a8aa792c
Amit kumar <codes.amitkumar@gmail.com>
2024-09-08 23:30:33 +0530

    file 1 add commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from 'path'
+import path from 'path';
 
 import connectToMongoDb from "./config/mongodb.js";
 import UserRoute from "./components/users/user.route.js";
@@ -14,31 +14,34 @@ dotenv.config();
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 5006;
-const app = new express();
-
-
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Client", "dist", "index.html"));
-});
+const app = express();
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:3000", // Restrict to your frontend's URL
-  credentials: true, // Enable sending of cookies with CORS
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  credentials: true,
 };
 
+// Middleware
 app.use(cors(corsOptions));
-
 app.use(express.json());
 app.use(cookieParser());
 
+// Serve static files from the client build directory
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+// Routes
 app.use("/api/users", UserRoute);
 app.use("/api/students", StudentRoute);
 app.use("/api/interview", InterviewRoute);
 app.use("/api/report", ReportRoute);
 
+// Catch-all route to serve the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+// Start the server and connect to MongoDB
 app.listen(PORT, () => {
   connectToMongoDb();
-  console.log("Server started succcessfully :", PORT);
+  console.log("Server started successfully on port :", PORT);
 });
